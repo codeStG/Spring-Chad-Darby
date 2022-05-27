023def3cc7ef999bcd0f3ed88c6c0dd81037a891
Torres <getorres@teksystems.com>
2022-05-27 11:35:43 -0500

    Set up dependency injection using constructor injection

--- src/com/stgcodes/springdemo/BasketballCoach.java
+++ src/com/stgcodes/springdemo/BasketballCoach.java
@@ -1,10 +1,23 @@
 package com.stgcodes.springdemo;
 
 public class BasketballCoach implements Coach {
+	
+	//define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	//define a constructor for dependency injection
+	public BasketballCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Shoot 30 baskets";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
--- src/com/stgcodes/springdemo/HelloSpringApp.java
+++ src/com/stgcodes/springdemo/HelloSpringApp.java
@@ -14,6 +14,9 @@ public class HelloSpringApp {
 		//call methods on the Bean
 		System.out.println(coach.getDailyWorkout());
 		
+		//call method for fortunes
+		System.out.println(coach.getDailyFortune());
+		
 		//close the context
 		context.close();
 	}
