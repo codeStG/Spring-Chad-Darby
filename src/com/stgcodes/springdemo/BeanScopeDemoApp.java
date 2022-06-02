6c2af67510d5189e0e79b96298a2fabd967fcb2d
Torres <getorres@teksystems.com>
2022-06-02 10:06:05 -0500

    Creating an example of using the prototype scope for a bean

---
+++ src/com/stgcodes/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,29 @@
+package com.stgcodes.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		//load the Spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		//retrieve Bean from Spring Container
+		Coach coach = context.getBean("coach", Coach.class);
+		
+		Coach coachTwo = context.getBean("coach", Coach.class);
+		
+		//check if they are the same
+		boolean result = (coach == coachTwo);
+		
+		//print out result
+		System.out.println("\nPointing to the same object: " + result);
+		System.out.println("\nMemory location for coach: " + coach);
+		System.out.println("\nMemory location for coachTwo: " + coachTwo + "\n");
+
+		//close the context
+		context.close();
+	}
+
+}
