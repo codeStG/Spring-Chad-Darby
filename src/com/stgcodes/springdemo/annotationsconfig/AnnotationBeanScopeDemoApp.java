1c3ddbd02f5b6aeb0fd15c95af83aa39b4cb18a5
Torres <getorres@teksystems.com>
2022-06-03 14:27:57 -0500

    Using annotations to set Bean Scope to prototype

---
+++ src/com/stgcodes/springdemo/annotationsconfig/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,31 @@
+package com.stgcodes.springdemo.annotationsconfig;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		//load spring config
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/stgcodes/springdemo/annotationsconfig/config/applicationContext.xml"); 
+		
+		//retrieve bean from spring container
+		Coach coach = context.getBean("tennisCoach", Coach.class);
+		
+		Coach coachTwo = context.getBean("tennisCoach", Coach.class);
+		
+		
+		//check if they are the same object
+		boolean result = (coach == coachTwo);
+		
+		//print out the result
+		System.out.println("\nPointing to the same object: " + result);
+		
+		System.out.println("\nMemory location for coach: " + coach);
+		
+		System.out.println("\nMemory location for coachTwo: " + coachTwo);
+		
+		//close the context
+		context.close();
+	}
+
+}
